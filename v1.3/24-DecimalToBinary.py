0fdd40c0d91f253e331130fd6c6fd269e475c330
TheHGB <holaemdichgb@gmail.com>
2018-11-01 20:31:45 +0100

    Added 24-DecimalToBinary.py 27-FibronacciSequence.py 28-Factorial.py

---
+++ v1.3/24-DecimalToBinary.py
@@ -0,0 +1,2 @@
+number = raw_input("Input the number you wish to convert: ")
+print "The number " + str(number) + " in binary is " + bin(int(number))[2::]
---
+++ v1.3/28-Factorial.py
@@ -0,0 +1,5 @@
+#This works better than any other implementation I could think of
+
+ 
+import math
+print math.factorial(int(raw_input("Enter the number you wish to factorize: ")))
